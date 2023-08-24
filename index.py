9719b0ced38d3647507b03449998277aa9dc9949
lchryu <87046628+lchryu@users.noreply.github.com>
2023-08-25 00:52:41 +0700

    load background image

--- index.py
+++ index.py
@@ -1,14 +1,16 @@
 import pygame
-
 pygame.init()
 screen = pygame.display.set_mode((400, 600))
 pygame.display.set_caption("Flappy Bird")
 clock = pygame.time.Clock()
 WHITE = (255, 255, 255)
+background_img = pygame.image.load('images/background.png')
+background_img = pygame.transform.scale(background_img, (400, 600))
 running = True
 while running:
     clock.tick(60)
     screen.fill(WHITE)
+    screen.blit(background_img, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
